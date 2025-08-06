c7e113a419812be0d9a9b028103e78a78bca35aa
Ivan Nolasco <ivannolasco@ravn.co>
2025-08-06 16:51:15 -0600

    chore: add favorite repository with its methods to be exposed

---
+++ app/src/main/java/com/example/pokedex/data/FavoriteRepository.kt
@@ -0,0 +1,24 @@
+package com.example.pokedex.data
+
+data class FavoritePokemon(val name: String)
+
+interface FavoriteRepository {
+    fun isFavorite(name: String): Boolean
+    fun toggleFavorite(name: String)
+}
+
+class FavoriteRepositoryImpl : FavoriteRepository {
+    private val favorites = mutableListOf<FavoritePokemon>()
+
+    override fun isFavorite(name: String): Boolean {
+        return favorites.contains(FavoritePokemon(name))
+    }
+
+    override fun toggleFavorite(name: String) {
+        if (!isFavorite(name)) {
+            favorites.add(FavoritePokemon(name))
+        } else {
+            favorites.remove(FavoritePokemon(name))
+        }
+    }
+}
